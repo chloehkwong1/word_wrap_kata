12bafb3b5a1f7421e4637421a0e936c1cf03748b
Derek Baker <derek.baker89@gmail.com>
2020-09-21 15:12:17 +0100

    deleted comments from production file

--- lib/word_wrap.rb
+++ lib/word_wrap.rb
@@ -11,23 +11,6 @@ class Wrapper
         end
         wrap_output << "#{split_word.shift}"
         return wrap_output
-       
-        
-    
-
-        # if split_word.length <= 1
-        #     return word
-        # end
-        # if split_word.length == 2
-        #     return "#{split_word[0]}\n#{split_word[1]}"
-        # end
-        # if split_word.length == 3
-        #     return "#{split_word[0]}\n#{split_word[1]}\n#{split_word[2]}"
-        # end
-        # if split_word.length == 4
-        #     return "#{split_word[0]}\n#{split_word[1]}\n#{split_word[2]}\n#{split_word[3]}"
-        # end
-
     end
 
 end

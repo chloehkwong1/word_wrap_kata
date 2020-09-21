81fd3dd58375271b7ffcab15e940d0c5c4b6513d
Chloe <chloe.wong@madetech.com>
2020-09-21 12:16:30 +0100

    failing tests due to transformation giving an extra new line on end of output

--- lib/word_wrap.rb
+++ lib/word_wrap.rb
@@ -2,19 +2,31 @@ class Wrapper
 
     def self.wrap(word, line_no)
         split_word = word.split
+        wrap_output = ""
 
-        if split_word.length <= 1
-            return word
+        while split_word.empty? == false
+            #puts "split_word: #{split_word}"
+            wrap_output << "#{split_word.shift}\n"
+            #puts "wrap_output: #{wrap_output}"
         end
-        if split_word.length == 2
-            return "#{split_word[0]}\n#{split_word[1]}"
-        end
-        if split_word.length == 3
-            return "#{split_word[0]}\n#{split_word[1]}\n#{split_word[2]}"
-        end
-        if split_word.length == 4
-            return "#{split_word[0]}\n#{split_word[1]}\n#{split_word[2]}\n#{split_word[3]}"
-        end
+        p "wrap_output: #{wrap_output}"
+        return wrap_output
+       
+        
+    
+
+        # if split_word.length <= 1
+        #     return word
+        # end
+        # if split_word.length == 2
+        #     return "#{split_word[0]}\n#{split_word[1]}"
+        # end
+        # if split_word.length == 3
+        #     return "#{split_word[0]}\n#{split_word[1]}\n#{split_word[2]}"
+        # end
+        # if split_word.length == 4
+        #     return "#{split_word[0]}\n#{split_word[1]}\n#{split_word[2]}\n#{split_word[3]}"
+        # end
 
     end
 

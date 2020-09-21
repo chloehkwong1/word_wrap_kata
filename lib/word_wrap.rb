b557bcc89c764885381f31348187b64a5593f911
Chloe <chloe.wong@madetech.com>
2020-09-21 11:22:34 +0100

    passes 3 word input same length test with sliming

--- lib/word_wrap.rb
+++ lib/word_wrap.rb
@@ -2,11 +2,17 @@ class Wrapper
 
     def self.wrap(word, line_no)
         split_word = word.split
-        
+
         if split_word.length <= 1
             return word
         end
-            "#{split_word[0]}\n#{split_word[1]}"
+        if split_word.length == 2
+            return "#{split_word[0]}\n#{split_word[1]}"
+        end
+        if split_word.length == 3
+            return "#{split_word[0]}\n#{split_word[1]}\n#{split_word[2]}"
+        end
+
     end
 
 end
--- spec/word_wrap_spec.rb
+++ spec/word_wrap_spec.rb
@@ -24,3 +24,9 @@ describe '2 word input where one word is the same length as column' do
         expect(Wrapper.wrap("hello derek", 5)).to eq("hello\nderek")
     end
 end
+
+describe '3 word input where all words are the same length as column' do
+    it 'returns "hello derek fives" on two lines when the column number is 5' do
+        expect(Wrapper.wrap("hello derek fives", 5)).to eq("hello\nderek\nfives")
+    end
+end
